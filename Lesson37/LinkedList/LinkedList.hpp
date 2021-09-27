a62ee47443214fc5d84846cf33b2b77177006dd3
SonaBadalyan <sona.badalyan.1992@gmail.com>
2021-09-28 01:23:21 +0400

    Added 37-th lesson task solution.

---
+++ Lesson37/LinkedList/LinkedList.hpp
@@ -0,0 +1,59 @@
+template<class T>
+class LinkedList
+{
+    class Node
+    {
+        public:
+
+            Node (T data)
+            {
+                m_data = data;
+            }
+
+            T m_data = 0;
+            Node* m_next = nullptr;
+            Node* m_prev = nullptr; 
+    };
+    
+    public:
+
+        LinkedList();
+
+        LinkedList(const LinkedList& rhs);
+
+        bool operator==(const LinkedList& rhs);
+
+        bool find(T val);
+
+        bool removeByValue(T value);
+
+        bool remove(int index);
+        
+        int operator[](int index);
+
+        bool deleteBack();
+
+        bool deleteFront();
+
+        void add(T data);
+
+        void addFront(T data);
+
+        bool insert(int index, T val);
+
+        bool isEmpty();
+
+        void print();
+
+        void reversePrint();
+
+        int length();
+
+        ~LinkedList();
+
+    private:
+
+        int m_length;
+        Node* head;
+        Node* tail;
+};
