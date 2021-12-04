4e4e90edbaea901fdf8dc47b3c797cc544b02a3c
SonaBadalyan <sona.badalyan.1992@gmail.com>
2021-12-04 15:36:19 +0400

    Lesson58

---
+++ C++/Lesson58/FindIfAlgorithm/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class Person
+{
+    public:
+
+        std::string name;
+        std::string surname;
+        int age;
+        int IQ;
+        int experiance;
+
+    public:
+
+        Person(std::string name, std::string surname, int age, int IQ, int experiance)
+        {
+            this->name = name;
+            this->surname = surname;
+            this->age = age;
+            this->IQ = IQ;
+            this->experiance = experiance;
+        }
+
+        Person() = default;
+
+        friend std::ostream& operator<<(std::ostream& output, const Person& p)
+        {
+            output << "__________________________________" << std::endl;
+            output << "Name : " << p.name << std::endl;
+            output << "Surname : " << p.surname << std::endl;
+            output << "Age : " << p.age << std::endl;
+            output << "IQ : " << p.IQ << std::endl;
+            output << "Experiance : " << p.experiance << std::endl;
+
+            return output;
+
+        }
+
+        friend bool operator==(const Person& p1, const Person& p2)
+        {
+            if (&p1 == &p2)
+            {
+                return true;
+            }
+
+            if (    
+                    p1.name == p2.name &&
+                    p1.surname == p2.surname &&
+                    p1.age == p2.age &&
+                    p1.IQ == p2.IQ && 
+                    p1.experiance == p2.experiance
+                )
+                {
+                    return true;
+                }
+
+            return false;
+        }
+
+};
+
+int main()
+{
+    std::vector<Person> vp;
+
+    vp.push_back(Person("Anna", "Abrahamyan", 22, 120, 3));
+    vp.push_back(Person("Anna", "Abrahamyan", 22, 120, 3));
+    vp.push_back(Person("Anna", "Abrahamyan", 22, 120, 3));
+    vp.push_back(Person("Asot", "Kirakosyan", 22, 120, 3));
+    vp.push_back(Person("Valod", "Mzmalyan", 22, 120, 3));
+    vp.push_back(Person("Vahan", "Khachatryan", 22, 120, 3));
+
+    auto i = std::find_if(vp.begin(), vp.end(), [](const Person& a){
+        return a.age > 22;
+    });
+
+    if ( i != vp.end())
+    {
+        std::cout << "Found " << *i <<std::endl;
+    }
+    else 
+    {
+        std::cout << "That person does not exist!" << std::endl;;
+    }
+    
+    return 0;
+}
